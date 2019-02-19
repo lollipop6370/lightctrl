ba7b81a813b2ef642ef84f9ebe92dfee03c0e27f
lollipop6370 <knn101do@gmail.com>
2019-02-20 00:41:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/webhook', function (req, res) {
     thisQs.light_id = "main";
     request({
         //uri: "https://sheetdb.io/api/v1/" + SHEETDB_PRODUCTINFO_ID + "/light_id/main",
-        uri: "https://sheetdb.io/api/v1/rqb9bz3aeg2qf/light_id/main",
+        url: "https://sheetdb.io/api/v1/rqb9bz3aeg2qf/light_id/main",
         json: true,
         method: "PUT",
         headers: { "Content-Type": "application/json" },
